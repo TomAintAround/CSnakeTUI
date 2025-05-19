6faae1dbda64ac9083aa8f3eb3baa7e1cdc7b11d
TomAintAround <tcorreia2005@proton.me>
2025-05-19 19:00:49 +0100

    chore: move functions from "common.c" to "main.c"

--- src/common.h
+++ src/common.h
@@ -14,22 +14,4 @@
  */
 void error(const char* errorMessage);
 
-/**
- * @brief Checks if 2 doubles are approximately the same value.
- * The order of the arguments are irrelevant.
- *
- * @param[in] number1 The first number.
- * @param[in] number1 The second number.
- * @return Whether the numbers are approximately the same or not.
- */
-bool close(const double number1, const double number2);
-
-/**
- * @brief Checks for user input and and change direction of the snake.
- *
- * @param[inout] dx Pointer of the difference in the x axis.
- * @param[inout] dy Pointer of the difference in the y axis.
- */
-void translateInput(int* dx, int* dy);
-
 #endif
--- src/main.c
+++ src/main.c
@@ -2,10 +2,38 @@
 #include <time.h>
 #include <ncurses.h>
 #include <locale.h>
-#include "snake.h"
+#include <math.h>
 #include "common.h"
+#include "main.h"
+#include "snake.h"
 #include "ui.h"
 
+bool close(const double number1, const double number2) {
+	if (fabs(number1 - number2) < 0.0000000001) return true;
+	return false;
+}
+
+void translateInput(int* dx, int* dy) {
+	double angles[4] = { -PI / 2, -PI, PI / 2, 0 };
+	double angle = angles[0];
+	int key = getch();
+	if (key == ERR) return;
+	if (key == 'w' || key == 'W' || key == KEY_UP) {
+		angle = angles[0];
+	} else if (key == 'a' || key == 'A' || key == KEY_LEFT) {
+		angle = angles[1];
+	} else if (key == 's' || key == 'S' || key == KEY_DOWN) {
+		angle = angles[2];
+	} else if (key == 'd' || key == 'D' || key == KEY_RIGHT) {
+		angle = angles[3];
+	}
+
+	// To avoid 180º turns
+	if (!close(*dx, -cos(angle)) || !close(*dy, -sin(angle))) {
+		*dx = cos(angle);
+		*dy = sin(angle);
+	}
+}
 int main() {
 	setlocale(LC_ALL, "");
 	initscr();
