8a0624470e2efd1e56f2f98020a6de113e8cde0e
TomAintAround <tcorreia2005@proton.me>
2025-05-20 16:26:17 +0100

    chore: move #defines from common.h to common.c

--- src/common.h
+++ src/common.h
@@ -1,12 +1,6 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#define INIT_NUM 3
-#define EASY_MODE 0.3
-#define MEDIUM_MODE 0.2
-#define HARD_MODE 0.1
-#define PI 3.14159265358979323846
-
 /**
  * @brief Exits the game with an error message and returns 1.
  *
--- src/main.h
+++ src/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define INIT_NUM 3
+#define EASY_MODE 0.3
+#define MEDIUM_MODE 0.2
+#define HARD_MODE 0.1
+#define PI 3.14159265358979323846
+
 /**
  * @brief Checks if 2 doubles are approximately the same value.
  * The order of the arguments are irrelevant.
